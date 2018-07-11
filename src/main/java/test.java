21c6eee9867bd1d0120fa72a2038cca293bc1236
hhz <877367304@qq.com>
2018-07-11 18:15:45 +0800

    "第五次提交"

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -5,12 +5,6 @@ public class test {
     private String name;
     private String password;
 
-    public void lianxi(){
-        System.out.println("好好学习，天天向上");
-    }
 
-    public void lianxi1(){
-        System.out.println("好好学习，天天向上");
-    }
 
 }
