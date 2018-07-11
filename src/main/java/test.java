f48b9ec18f59f9c7e990b2d5ea39e1a62647d9f5
hhz <877367304@qq.com>
2018-07-11 19:16:30 +0800

    "第九次次提交"

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -3,6 +3,17 @@
  */
 public class test {
     private String name;
+    public  void lianxi2(){
+        System.out.println("dssd");
+    } public  void lianxi1(){
+        System.out.println("dssd");
+    } public  void lianxi3(){
+        System.out.println("dssd");
+    } public  void lianxi4(){
+        System.out.println("dssd");
+    } public  void lianxi5(){
+        System.out.println("dssd");
+    }
 
 
 
