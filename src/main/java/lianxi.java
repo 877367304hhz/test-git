cc5da97bfc4cc46359e71870f3ebabe2f45b4420
hhz <877367304@qq.com>
2018-07-11 17:47:01 +0800

    "第二次提交"

--- src/main/java/lianxi.java
+++ src/main/java/lianxi.java
@@ -2,4 +2,40 @@
  * Created by Administrator on 2018/7/11 0011.
  */
 public class lianxi {
+    private String name;
+    private String password;
+
+
+    public lianxi() {
+    }
+
+    @Override
+    public String toString() {
+        return "lianxi{" +
+                "name='" + name + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public lianxi(String name, String password) {
+
+        this.name = name;
+        this.password = password;
+    }
 }
