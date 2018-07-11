09bbd8f7d8a320ab06695cf48ae822700685a1c5
hhz <877367304@qq.com>
2018-07-11 18:01:53 +0800

    "第四次提交"

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -5,5 +5,12 @@ public class test {
     private String name;
     private String password;
 
-    //好好学习，天天向上
+    public void lianxi(){
+        System.out.println("好好学习，天天向上");
+    }
+
+    public void lianxi1(){
+        System.out.println("好好学习，天天向上");
+    }
+
 }
